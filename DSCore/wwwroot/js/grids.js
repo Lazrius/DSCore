45208499618b74ca9f8534922ebb9a8b601409c2
Laz <lazdisco@gmail.com>
2019-01-27 22:49:13 +0000

    Terrible solution is terrible

--- DSCore/wwwroot/js/grids.js
+++ DSCore/wwwroot/js/grids.js
@@ -1,8 +1,8 @@
 ﻿const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-const comparer = (idx, asc) => (a, b) => ((v1, v2) => (
-    v1 !== "" && v2 !== "" && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2))
-)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+    v1 !== "" && v2 !== "" && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+)(getCellValue(asc ? a : b, idx).replace(/\,/g, "").replace(/\$/g, ""), getCellValue(asc ? b : a, idx).replace(/\,/g, "").replace(/\$/g, ""));
 
 // do the work...
 document.getElementById("tbl-header").querySelectorAll("td").forEach(td => td.addEventListener("click", (() => {
