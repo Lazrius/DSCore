16afae3349a57432b42225da1971f6c9c2ff976b
LazDisco <lazdisco@gmail.com>
2019-01-28 11:54:52 +0000

    Cloaks and Disrupters finished. Might need to find a better way to display disrupters, perhaps load in the CM list.

---
+++ DSCore/Controllers/CloakController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DSCore.Ini;
+using Microsoft.AspNetCore.Mvc;
+using DSCore.Models;
+using DSCore.Utilities;
+
+namespace DSCore.Controllers
+{
+    [Route("[controller]")]
+    public class CloakController : InheritController
+    {
+        public IActionResult Cloaks()
+        {
+            Errors error = Errors.Null;
+            try
+            {
+                var cloaks = Utils.GetDatabaseCollection<CloakingDevice>("Cloaks", ref error);
+                if (error != Errors.Null)
+                    throw new InvalidOperationException("The database was unable to access the Cloaks collection.");
+
+                var infocards = Utils.GetDatabaseCollection<Infocard>("Infocards", ref error);
+                if (error != Errors.Null)
+                    throw new InvalidOperationException("The database was unable to access the Infocards collection.");
+
+                ViewBag.Infocards = infocards;
+                return View("Cloak", cloaks);
+            }
+            catch (Exception ex)
+            {
+                return View("DatabaseError", new PageException(ex, error));
+            }
+        }
+
+        [HttpGet("disrupter")]
+        public IActionResult Disrupters()
+        {
+            Errors error = Errors.Null;
+            try
+            {
+                var disrupters = Utils.GetDatabaseCollection<CloakDisrupter>("Disrupters", ref error);
+                if (error != Errors.Null)
+                    throw new InvalidOperationException("The database was unable to access the Cloaks collection.");
+
+                var infocards = Utils.GetDatabaseCollection<Infocard>("Infocards", ref error);
+                if (error != Errors.Null)
+                    throw new InvalidOperationException("The database was unable to access the Infocards collection.");
+
+                ViewBag.Infocards = infocards;
+                return View("Disrupter", disrupters);
+            }
+            catch (Exception ex)
+            {
+                return View("DatabaseError", new PageException(ex, error));
+            }
+        }
+    }
+}
