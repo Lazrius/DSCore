472740e7d15b4d4b9b49d1cfe519b6ab75e769cc
LazDisco <lazdisco@gmail.com>
2019-02-12 10:39:17 +0000

    Reworked systems to open up in the online navmap. Removed ooRP/inaccessible systems from the list.

--- DSCore/Controllers/SystemController.cs
+++ DSCore/Controllers/SystemController.cs
@@ -31,34 +31,5 @@ namespace DSCore.Controllers
                 return View("DatabaseError", new PageException(ex, error));
             }
         }
-
-        [HttpGet("{nickname}")]
-        public IActionResult Individual(string nickname)
-        {
-            Errors error = Errors.Null;
-            try
-            {
-                var systems = Utils.GetDatabaseCollection<Ini.System>("Systems", ref error);
-                if (error != Errors.Null)
-                    throw new InvalidOperationException("The database was unable to access the Systems collection.");
-
-                var infocards = Utils.GetDatabaseCollection<Infocard>("Infocards", ref error);
-                if (error != Errors.Null)
-                    throw new InvalidOperationException("The database was unable to access the Infocards collection.");
-
-                var factions = Utils.GetDatabaseCollection<Faction>("Factions", ref error);
-                if (error != Errors.Null)
-                    throw new InvalidOperationException("The database was unable to access the Factions collection.");
-
-                Ini.System system = systems.Find(x => x.Nickname == nickname);
-                ViewBag.Factions = factions;
-                ViewBag.Infocards = infocards;
-                return View(system);
-            }
-            catch (Exception ex)
-            {
-                return View("DatabaseError", new PageException(ex, error));
-            }
-        }
     }
 }
